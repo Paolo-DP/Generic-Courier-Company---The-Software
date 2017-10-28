d08c1ce1c70f3ba224786b93dd593d892c10c2e9
Lenovo <Lenovo@LAPTOP-1HBEJFSN>
2017-10-28 17:42:55 +0800

    Set up initial files

---
+++ src/generic_courier_company_the_software/Generic_Courier_Company_The_Software.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generic_courier_company_the_software;
+
+/*
+Authors:
+Paolo Dela Pena
+Jashandeep Bhuller
+Jan Reece Reyes
+Julio Simeon
+ */
+public class Generic_Courier_Company_The_Software {
+
+    /*
+    This is the Main Class that will launch the different forms
+    In the Final Program, it will launch the setup program first
+    to select which side of the software (customer, hub, delivery)
+    will be active
+    
+    After set up, the selected software will run automatically
+    the next time the software is ran
+    */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
