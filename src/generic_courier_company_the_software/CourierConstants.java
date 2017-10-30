dd45f7158669976754550b45f7092e3583db2486
Lenovo <Lenovo@LAPTOP-1HBEJFSN>
2017-10-31 00:44:18 +0800

    added new package form

---
+++ src/generic_courier_company_the_software/CourierConstants.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generic_courier_company_the_software;
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface CourierConstants {
+    public final String database_url = "jdbc:mysql://localhost/courierservice";
+}
